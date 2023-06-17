a8aa7ec058f5d0f286ec047e279f9e709c156d7c
Shashank Maurya <shashankmaurya260101@gmail.com>
2023-06-18 02:47:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const baseURL = "https://us21.api.mailchimp.com/3.0/";
+
+// hide these before pushing to github
+const list_id = '3011aaff57';
+const api_key = '0baf4246b02b958d4cf3647bab71-us21';
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.htm");
+});
+
+app.post('/', function (req, res) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+
+    console.log(fname, lname, email);
+
+    let data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    let url = baseURL + 'lists/' + list_id;
+    let options = {
+        method: 'POST',
+        auth: 'shashank01:' + api_key
+    }
+
+    const request = https.request(url, options, function (response) {
+        console.log('status code : ', response.statusCode);
+        console.log('response headers : ', response.headers);
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.htm');
+        }
+        else {
+            res.sendFile(__dirname + "/failure.htm");
+        }
+
+        response.on('data', function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.on('error', (err) => console.error(err));
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post('/failure', function(req, res){
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('server started at port 3000'));
